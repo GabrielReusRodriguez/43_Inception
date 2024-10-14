80ab2e112c4ea517766b7d558bb00bae7db54250
Gabriel Reus <Gabriel.Reus@gmail.com>
2024-10-14 23:11:52 +0200

    Avances

--- project/srcs/requirements/nginx/tools/nginx_config.sh
+++ project/srcs/requirements/nginx/tools/nginx_config.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
 # First we update the apt repositories
-sudo apt update 
+apt update 
 
 # Second we install nginx
-sudo apt install nginx 
+apt install nginx -y
 
 #Third we install openssl
-sudo apt install openssl
+apt install openssl -y
 
 #Then we edit nginx.conf to add TSLv1.2
 echo "-----
 
 ssl_protocols TLSv1.2 TLSv1.3;
-
 ssl_ciphers HIGH:!aNULL:!MD5;
 ssl_prefer_server_ciphers on;
 
