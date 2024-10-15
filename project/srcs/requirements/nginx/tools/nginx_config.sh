2e14752bbbec722d192556c5eafc7ebb8237a207
Gabriel Reus <Gabriel.Reus@gmail.com>
2024-10-15 21:10:39 +0200

    Ya conecta nginx

--- project/srcs/requirements/nginx/tools/nginx_config.sh
+++ project/srcs/requirements/nginx/tools/nginx_config.sh
@@ -9,11 +9,32 @@ apt install nginx -y
 #Third we install openssl
 apt install openssl -y
 
+#Config of nginx.
+echo "
+http
+{
+	server 
+	{
+
+		listen 80
+		root /var/www/html/
+		location /
+		{
+			index index.htm index.html
+		}
+		location ~\.php
+		{
+		}
+
+	}
+}" > /etc/nginx/nginx.conf
+
+
 #Then we edit nginx.conf to add TSLv1.2
-echo "-----
-
-ssl_protocols TLSv1.2 TLSv1.3;
-ssl_ciphers HIGH:!aNULL:!MD5;
-ssl_prefer_server_ciphers on;
-
------"  >> /etc/nginx/nginx.conf
+#echo "-----
+#
+#ssl_protocols TLSv1.2 TLSv1.3;
+#ssl_ciphers HIGH:!aNULL:!MD5;
+#ssl_prefer_server_ciphers on;
+#
+#-----"  >> /etc/nginx/nginx.conf
