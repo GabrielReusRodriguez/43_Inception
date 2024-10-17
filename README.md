e7063fb992935c650da3bb319c59d390af9b828a
Gabriel Reus <Gabriel.Reus@gmail.com>
2024-10-17 23:45:56 +0200

    fin por hoy

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 43_Inception
 Project Inceptuion from Badalona 43
+
+https://github.com/vbachele/Inception/blob/main/scrs/docker-compose.yml
--- project/srcs/requirements/nginx/tools/nginx_config.sh
+++ project/srcs/requirements/nginx/tools/nginx_config.sh
@@ -8,3 +8,10 @@ apt install nginx -y
 
 #Third we install openssl
 apt install openssl -y
+
+mkdir -p /etc/nginx/ssl
+
+openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes \
+	-out /etc/nginx/ssl/greus-ro.crt \
+	-keyout /etc/nginx/ssl/greus-ro.key \
+	-subj "/C=ES/ST=Barcelona/L=Barcelona/O=42 School/OU=greus-ro/CN=greus-ro"
