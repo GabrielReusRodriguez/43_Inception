21d4aa8756c25bd62d5356e779951cb281087720
Gabriel Reus <Gabriel.Reus@gmail.com>
2024-10-21 20:04:38 +0200

    Nginx ya funciona

--- project/srcs/requirements/nginx/tools/nginx_config.sh
+++ project/srcs/requirements/nginx/tools/nginx_config.sh
@@ -14,3 +14,9 @@ openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes \
 	-out /etc/nginx/ssl/greus-ro.crt \
 	-keyout /etc/nginx/ssl/greus-ro.key \
 	-subj "/C=ES/ST=Barcelona/L=Barcelona/O=42 School/OU=greus-ro/CN=greus-ro"
+
+#Changing owner of data to allow execu with user www-data,not root.
+#Executing with root is NOT secure.
+#chown  www-data /etc/nginx/ssl/greus-ro.crt
+#chown  www-data /etc/nginx/ssl/greus-ro.key
+#chown  --recursive  www-data  /var/lib/nginx
